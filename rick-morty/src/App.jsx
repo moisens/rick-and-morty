e389392bed7e0c974717c334b8b69191f33e6e16
moise <mose.ns@hotmail.com>
2021-10-05 19:25:26 +0200

    rick part2

--- rick-morty/src/App.jsx
+++ rick-morty/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.scss'
 import Header from './components/header/Header';
-import Aside from './components/aside/Aside';
+
 
 function App() {
 
   return (
     <div className='app-container'>
       <Header />
-      <Aside />
     </div>
   )
 }
